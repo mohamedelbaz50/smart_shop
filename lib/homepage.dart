8b04b15b7dca5fcccad61c52f5e3ccce5f2dde14
mohamedelbaz50 <aboelbaz611@gmail.com>
2024-02-28 18:23:22 +0200

    ة

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,6 +12,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String _barcode = "";
+  
   // ignore:
   ProductModel? productModel; // Initialize with null value
 
